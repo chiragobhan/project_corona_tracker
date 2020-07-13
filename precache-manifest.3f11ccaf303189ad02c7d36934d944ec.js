b0c1674e01a49cf8c266e75cae957035c87eee23
Chirag Obhan <32812640+chiragobhan@users.noreply.github.com>
2020-07-14 02:01:29 +0530

    Updates

---
+++ precache-manifest.3f11ccaf303189ad02c7d36934d944ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f42b5635da4c2390ade57a27953eca2",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "c7bdd243a1c937bdfebc",
+    "url": "/react-covid-tracker/static/css/main.c1d92c9f.chunk.css"
+  },
+  {
+    "revision": "4ce074fd6ad5bed969c2",
+    "url": "/react-covid-tracker/static/js/2.d8829c47.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-tracker/static/js/2.d8829c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bdd243a1c937bdfebc",
+    "url": "/react-covid-tracker/static/js/main.73d0a613.chunk.js"
+  },
+  {
+    "revision": "63cbadfa6a844bfb299a",
+    "url": "/react-covid-tracker/static/js/runtime-main.bfb9ab11.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid-tracker/static/media/image.d7265326.png"
+  }
+]);
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Chart } from './Chart/Chart';
-export { default as CountryPicker } from './CountryPicker/CountryPicker';
-export { default as StateTable } from './StateTable/StateTable';
-export { default as Cards } from './Cards/Cards';
